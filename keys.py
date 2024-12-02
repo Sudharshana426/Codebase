e56cc187452006f877dbacfe1083a2f4dcf9b6ec
Sudharshana Balaji <142670962+Sudharshana426@users.noreply.github.com>
2024-12-02 14:56:14 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,11 @@
+import os
+
+# Set up API keys and configuration
+PINECONE_API_KEY = "pcsk_61K8wm_GtSP78vcpTcBGHSqqeEyz3TbBBZKkmn7Twj2T3MER6WSCkkpMzeS1pMr7Zh928y"
+GROQ_API_KEY = "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"
+
+PINECONE_ENVIRONMENT = 'us-east-1'
+PINECONE_INDEX_NAME = 'codebase-rag'
+PINECONE_NAMESPACE = 'https://github.com/CoderAgent/SecureAgent'
+
+GROQ_MODEL = "llama-3.1-70b-versatile"
